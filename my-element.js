f5f723d6cfc469f6139ae46ebefc37c68a151b86
Álvaro Touzón <alvaro.touzon@gmail.com>
2019-08-21 11:07:17 +0200

    te he puesto comentarios

--- my-element.js
+++ my-element.js
@@ -14,7 +14,17 @@ class MyElement extends LitElement {
         }
       </style>
       <!-- template content -->
-      
+      /*
+      En este caso necesitamos  el data-bind(https://lit-element.polymer-project.org/guide/templates#bind-properties-to-child-elements)
+      a un evento @input el input es nativo de js(https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event),
+      mejor que al changed
+      pero tambien podemos bindear .value de forma directa a una property todo
+      <input
+        type="text"
+        .value=${this.todo}
+        @input=${this.handleInput}
+      />
+      */
       <input type="text" id="textbox22" value="Change the paragraph" @change="${this.handleChange}"/>
       <p id="paragraph">A paragraph</p>
     `;
@@ -24,7 +34,10 @@ class MyElement extends LitElement {
     console.log("asdf2");
     console.log("En null:"+document.getElementById("textbox22"));
     console.log("Error:"+document.getElementById("textbox22").value);
+    /// aquí es donde comienza el data-bind 
+    /// https://www.wintellect.com/data-binding-pure-javascript/
     document.getElementById("paragraph").innerHTML=document.getElementById("textbox22").value;
+    
   }
 }
 // Register the new element with the browser.
